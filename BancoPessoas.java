64df0d0d0f25987c980a6a0c19ab1a00bfdceb88
nataliavitoriaamaral <natalia.amaraldoval@gmail.com>
2026-06-23 16:53:28 -0300

    Estrutura inicial do BancoPessoas com os dois HashMaps

---
+++ BancoPessoas.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class BancoPessoas {
+    // banco em memoria
+    private static HashMap<Integer, Pessoa> banco = new HashMap<>();
+    
+    // mapa que vai associar a string do comando com a ação dele
+    private static HashMap<String, Comando> comandos = new HashMap<>();
+
+    public static void main(String[] args) {
+    
+    }
+}
